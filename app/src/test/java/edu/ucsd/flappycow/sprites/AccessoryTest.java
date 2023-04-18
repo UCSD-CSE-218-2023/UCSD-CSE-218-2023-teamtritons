83d7982775f008e0a8385d43074f52c4181a9cf7
SIMRAN GILL <gillsimu98@gmail.com>
2023-04-17 17:27:21 -0700

    test cases commit

--- app/src/test/java/edu/ucsd/flappycow/sprites/AccessoryTest.java
+++ app/src/test/java/edu/ucsd/flappycow/sprites/AccessoryTest.java
@@ -3,7 +3,6 @@ package edu.ucsd.flappycow;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import android.graphics.Canvas;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.internal.AndroidTestEnvironment;
 
@@ -13,7 +12,6 @@ import static org.mockito.Mockito.mock;
 
 import android.app.Application;
 import android.content.Context;
-import android.graphics.Canvas;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
@@ -24,26 +22,28 @@ import edu.ucsd.flappycow.GameActivity;
 import edu.ucsd.flappycow.GameView;
 import edu.ucsd.flappycow.MainActivity;
 import edu.ucsd.flappycow.sprites.Accessory;
-import edu.ucsd.flappycow.sprites.Cow;
 
 @RunWith(AndroidJUnit4.class)
-public class CowTest {
+public class AccessoryTest {
 
     @Test
-    public void initCowObject(){
-        GameActivity gameActivity = Robolectric.setupActivity(GameActivity.class);
-        GameView gameView = new GameView(gameActivity);
-        Cow tCow = new Cow(gameView, gameActivity, new Accessory(gameView, gameActivity));
-//        assertFalse(tAccessory.getX() == 0 && tAccessory.getY() == 0);
-//        assertTrue(tAccessory.getX() == 3 && tAccessory.getY() == 4);
-    }
-
-    @Test
-    public void checkTouchingSpider(){
+    public void initAccessoryObject(){
         GameActivity gameActivity = Robolectric.setupActivity(GameActivity.class);
         GameView gameView = new GameView(gameActivity);
         Accessory tAccessory = new Accessory(gameView, gameActivity);
-        assertTrue(tAccessory.isTouching(1, 1));
+        tAccessory.moveTo(3,4);
+        assertFalse(tAccessory.getX() == 0 && tAccessory.getY() == 0);
+        assertTrue(tAccessory.getX() == 3 && tAccessory.getY() == 4);
+    }
+
+    @Test
+    public void checkMoveAccessory(){
+        GameActivity gameActivity = Robolectric.setupActivity(GameActivity.class);
+        GameView gameView = new GameView(gameActivity);
+        Accessory tAccessory = new Accessory(gameView, gameActivity);
+        tAccessory.moveTo(10, 11);
+        assertFalse(tAccessory.getX() == 3 && tAccessory.getY() == 4);
+        assertTrue(tAccessory.getX() == 10 && tAccessory.getY() == 11);
     }
 
 }
