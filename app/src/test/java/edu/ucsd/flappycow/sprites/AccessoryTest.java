f581f1c5c3101f9972bf09349ad6163382ef0a24
SIMRAN GILL <gillsimu98@gmail.com>
2023-04-17 17:27:31 -0700

    Test cases commit 2

--- app/src/test/java/edu/ucsd/flappycow/sprites/AccessoryTest.java
+++ app/src/test/java/edu/ucsd/flappycow/sprites/AccessoryTest.java
@@ -1,27 +1,13 @@
-package edu.ucsd.flappycow;
+package edu.ucsd.flappycow.sprites;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.android.internal.AndroidTestEnvironment;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-
-import android.app.Application;
-import android.content.Context;
-
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-
 import edu.ucsd.flappycow.GameActivity;
 import edu.ucsd.flappycow.GameView;
-import edu.ucsd.flappycow.MainActivity;
-import edu.ucsd.flappycow.sprites.Accessory;
 
 @RunWith(AndroidJUnit4.class)
 public class AccessoryTest {
