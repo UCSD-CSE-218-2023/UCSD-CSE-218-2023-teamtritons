7375f4f2cd1106b6b0a35d00a126561ed7706002
SIMRAN GILL <gillsimu98@gmail.com>
2023-04-23 20:37:35 -0700

    Impl the Observer Pattern for GameActivityHandler

---
+++ app/src/main/java/edu/ucsd/flappycow/GameActivityHandlerSubjectImpl.java
@@ -0,0 +1,2 @@
+package edu.ucsd.flappycow;public class GameActivityHandlerSubjectImpl {
+}
---
+++ app/src/main/java/edu/ucsd/flappycow/GameActivityHandlerUpdate.java
@@ -0,0 +1,9 @@
+package edu.ucsd.flappycow;
+
+import android.os.Message;
+
+public class GameActivityHandlerPojo {
+    GameActivity gameActivity;
+    Message msg;
+    int msgType;
+}
