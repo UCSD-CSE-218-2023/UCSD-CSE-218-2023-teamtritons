c01953b4df18e68559b6d7e13d816bae8afd1609
sakshisubedi <sakshisubedi@gmail.com>
2023-04-23 18:07:46 -0700

    tutorial

--- app/src/main/java/edu/ucsd/flappycow/sprite/Tutorial.java
+++ app/src/main/java/edu/ucsd/flappycow/sprite/Tutorial.java
@@ -1,4 +1,35 @@
 package edu.ucsd.flappycow.sprite;
 
-public class Tutorial {
+
+import android.graphics.Bitmap;
+
+import edu.ucsd.flappycow.GameActivity;
+import edu.ucsd.flappycow.GameView;
+import edu.ucsd.flappycow.R;
+import edu.ucsd.flappycow.Util;
+import edu.ucsd.flappycow.sprite.Sprite;
+
+public class Tutorial extends Sprite {
+    public static Bitmap globalBitmap;
+
+    public Tutorial(GameView view, GameActivity gameActivity) {
+        super(view, gameActivity);
+        if (globalBitmap == null) {
+            globalBitmap = Util.getScaledBitmapAlpha8(gameActivity, R.drawable.tutorial);
+        }
+        this.setBitmap(globalBitmap);
+        this.setWidth(this.getBitmap().getWidth());
+        this.setHeight(this.getBitmap().getHeight());
+    }
+
+    /**
+     * Sets the position to the center of the view.
+     */
+    @Override
+    public void move() {
+        this.setX(this.getView().getWidth() / 2 - this.getWidth() / 2);
+        this.setY(this.getView().getHeight() / 2 - this.getHeight() / 2);
+    }
+
 }
+
