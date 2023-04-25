5a87edf4a0ec6365ddf87f54baa8d1352f76bdb7
sakshisubedi <sakshisubedi@gmail.com>
2023-04-24 19:52:54 -0700

    test: achievementBox

---
+++ app/src/test/java/edu/ucsd/flappycow/AchievementBoxTest.java
@@ -0,0 +1,28 @@
+package edu.ucsd.flappycow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import edu.ucsd.flappycow.consts.ApplicationConstants;
+
+@RunWith(AndroidJUnit4.class)
+public class AchievementBoxTest {
+
+    @Test
+    public void onUpdateTest(){
+        AchievementBoxUpdate mockData = new AchievementBoxUpdate(ApplicationConstants.POINTS, "2");
+        AchievementBox achievementBox = new AchievementBox();
+        achievementBox.setPoints(1);
+
+        achievementBox.onUpdate(mockData);
+
+        assertTrue(achievementBox.getPoints() == 2);
+        assertThat(achievementBox.getPoints(), is(instanceOf(Integer.class)));
+    }
+}
