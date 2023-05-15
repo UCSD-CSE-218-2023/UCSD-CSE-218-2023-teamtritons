1d36c4fd518fa6213f5fc3372453e64720c3b5ec
twinkle <twinkle@twinkles-MacBook-Pro.local>
2023-05-15 12:32:31 -0700

    Updated test cases

--- app/src/test/java/edu/ucsd/flappycow/model/AccessoryTest.java
+++ app/src/test/java/edu/ucsd/flappycow/model/AccessoryTest.java
@@ -6,6 +6,8 @@ import org.robolectric.Robolectric;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import edu.ucsd.flappycow.factory.AccessoryFactory;
 import edu.ucsd.flappycow.view.GameActivity;
 import edu.ucsd.flappycow.view.GameView;
 
@@ -14,7 +16,7 @@ public class AccessoryTest {
 
     @Test
     public void initAccessoryObject(){
-        Accessory tAccessory = new Accessory();
+        Accessory tAccessory = (Accessory) AccessoryFactory.getInstance(edu.ucsd.flappycow.enums.Accessory.ACCESSORY);
         tAccessory.moveTo(3,4);
         assertFalse(tAccessory.getX() == 0 && tAccessory.getY() == 0);
         assertTrue(tAccessory.getX() == 3 && tAccessory.getY() == 4);
@@ -22,7 +24,7 @@ public class AccessoryTest {
 
     @Test
     public void checkMoveAccessory(){
-        Accessory tAccessory = new Accessory();
+        Accessory tAccessory = (Accessory) AccessoryFactory.getInstance(edu.ucsd.flappycow.enums.Accessory.ACCESSORY);
         tAccessory.moveTo(10, 11);
         assertFalse(tAccessory.getX() == 3 && tAccessory.getY() == 4);
         assertTrue(tAccessory.getX() == 10 && tAccessory.getY() == 11);
