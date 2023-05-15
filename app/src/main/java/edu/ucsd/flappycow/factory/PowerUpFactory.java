d952121725a4a4fc4e489d521fcc02f708e25f84
SIMRAN GILL <gillsimu98@gmail.com>
2023-05-15 10:49:30 -0700

    Fixed PowerUpFactory

--- app/src/main/java/edu/ucsd/flappycow/factory/PowerUpFactory.java
+++ app/src/main/java/edu/ucsd/flappycow/factory/PowerUpFactory.java
@@ -6,21 +6,20 @@ import edu.ucsd.flappycow.model.Accessory;
 import edu.ucsd.flappycow.model.Coin;
 import edu.ucsd.flappycow.model.Cow;
 import edu.ucsd.flappycow.model.IPlayableCharacter;
-import edu.ucsd.flappycow.model.IPowerUp;
 import edu.ucsd.flappycow.model.NyanCat;
 import edu.ucsd.flappycow.model.Rainbow;
 import edu.ucsd.flappycow.model.Toast;
 import edu.ucsd.flappycow.model.Virus;
 
 public class PowerUpFactory {
-    public static IPowerUp getInstance(PowerUp type, int speedX, int viewWidth) {
-        IPowerUp powerUp = null;
+    public static edu.ucsd.flappycow.model.PowerUp getInstance(PowerUp type, int speedX, int viewWidth) {
+        edu.ucsd.flappycow.model.PowerUp powerUp = null;
         if(type.equals(PowerUp.COIN)) {
-            powerUp = (IPowerUp) new Coin(speedX, viewWidth);
+            powerUp = new Coin(speedX, viewWidth);
         } else if (type.equals(PowerUp.TOAST)) {
-            powerUp = (IPowerUp) new Toast(speedX, viewWidth);
+            powerUp = new Toast(speedX, viewWidth);
         } else if (type.equals(PowerUp.VIRUS)) {
-            powerUp = (IPowerUp) new Virus(speedX, viewWidth);
+            powerUp = new Virus(speedX, viewWidth);
         }
         return powerUp;
     }
